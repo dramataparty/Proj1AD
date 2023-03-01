fef9cc4671edc9267063583a91c5fdde79ab54b1
dramataparty <101320231+dramataparty@users.noreply.github.com>
2023-03-01 13:42:32 +0000

    Create sock_utils.py

---
+++ sock_utils.py
@@ -0,0 +1,9 @@
+class server_connection:
+def __init__(self, address, port):
+pass # Remover esta linha e fazer implementação da função
+def connect(self):
+pass # Remover esta linha e fazer implementação da função
+def send_receive(self, data):
+pass # Remover esta linha e fazer implementação da função
+def close(self):
+pass # Remover esta linha e fazer implementação da função
