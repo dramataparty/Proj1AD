688447aeaf6f7e75a29bbd32bc4aaae8e33f3636
Dramata <diogobailaoforte@gmail.com>
2023-03-02 11:28:03 +0000

    indent fixes + stuff

--- sock_utils.py
+++ sock_utils.py
@@ -1,9 +1,16 @@
 class server_connection:
-def __init__(self, address, port):
-pass # Remover esta linha e fazer implementação da função
-def connect(self):
-pass # Remover esta linha e fazer implementação da função
-def send_receive(self, data):
-pass # Remover esta linha e fazer implementação da função
-def close(self):
-pass # Remover esta linha e fazer implementação da função
+    
+    def __init__(self, address, port):
+        HOST = address
+        PORT = port
+    pass # Remover esta linha e fazer implementação da função
+
+    def connect(self):
+    pass # Remover esta linha e fazer implementação da função
+
+    def send_receive(self, data):
+    pass # Remover esta linha e fazer implementação da função
+
+    def close(self):
+    pass # Remover esta linha e fazer implementação da função
+
