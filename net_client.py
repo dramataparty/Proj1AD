7580e2f62b8ad75f44eba7395ea2adaf27cb41bc
Dramata <diogobailaoforte@gmail.com>
2023-03-03 13:24:52 +0000

    correction progress

--- net_client.py
+++ net_client.py
@@ -1,43 +1,35 @@
 import sys, socket as s
-import sock_utils
 class server_connection:
-
+    conn_sock = s.socket(s.AF_INET, s.SOCK_STREAM)
     def __init__(self, address, port):
-        self.address = address
-        self.port = port
         
+        if len(sys.argv) > 1:
+            self.address = address
+            self.port = port   
+        else:
+            self.address = '127.0.0.1'
+            self.port = 9999
+                
     pass # Remover esta linha e fazer implementação da função
+    
 
     def connect(self):
+        conn_sock.connect((self.address, self.port))
+        while True:
+            msg = str(input('comando >'));
+            if msg == 'EXIT':
+                break
     pass # Remover esta linha e fazer implementação da função
 
     def send_receive(self, data):
+        conn_sock.sendall(msg.encode('utf-8'))
+        resposta = conn_sock.recv(1024)
+        print("resposta: " + resposta.decode())
     pass # Remover esta linha e fazer implementação da função
 
     def close(self):
+        conn_sock.close()    
     pass # Remover esta linha e fazer implementação da função
 
 
 
-if len(sys.argv) > 1:
-    HOST = sys.argv[1]
-    PORT = int(sys.argv[2])
-else:
-    HOST = '127.0.0.1'
-    PORT = 9999
-while True:
-    msg = str(input('comando >'));
-    if msg == 'EXIT':
-        break
-    
-    
-
-    conn_sock = s.socket(s.AF_INET, s.SOCK_STREAM)
-    conn_sock.connect((HOST, PORT))
-    conn_sock.sendall(msg.encode('utf-8'))
-    resposta = conn_sock.recv(1024)
-    print("resposta: " + resposta.decode())
-
-
-    
-conn_sock.close()
